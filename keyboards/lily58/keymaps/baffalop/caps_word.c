eba677c5317eba7edf9d0861112fcb36c8dabf2a
Nikita Gaidakov <ngaidakov@podfather.com>
2021-03-15 02:11:18 +0000

    Implement x-case instead of snake case: dynamically defined separator

--- keyboards/lily58/keymaps/baffalop/caps_word.c
+++ keyboards/lily58/keymaps/baffalop/caps_word.c
@@ -12,8 +12,18 @@
 // if caps word is true then CAPITALIZE next word
 // if snake case enabled then_replace_spaces_with_underscores
 // if both true THEN_SCREAMING_SNAKE_CASE_ALL_TEXT
+
+#define DEFAULT_SEPARATOR KC_UNDS
+
+enum x_case_state {
+    X_CASE_DISABLED,
+    AWAITING_SEPARATOR,
+    X_CASE_ACTIVE,
+};
+
 static bool caps_word_enabled = false;
-static bool snake_case_enabled = false;
+static uint8_t x_case_state = X_CASE_DISABLED;
+static uint16_t separator = DEFAULT_SEPARATOR;
 
 void enable_caps_word(void) {
     caps_word_enabled = true;
@@ -22,17 +32,26 @@ void enable_caps_word(void) {
     }
 }
 
-void enable_snake_case(void) {
-    snake_case_enabled = true;
+void enable_x_case(void) {
+    x_case_state = AWAITING_SEPARATOR;
 }
 
-void enable_screaming_snake_case(void) {
+void disable_x_case(void) {
+    x_case_state = X_CASE_DISABLED;
+    separator = DEFAULT_SEPARATOR;
+}
+
+bool is_x_case_enabled(void) {
+    return x_case_state != X_CASE_DISABLED;
+}
+
+void enable_screaming_x_case(void) {
     enable_caps_word();
-    enable_snake_case();
+    enable_x_case();
 }
 
 bool is_caps_anything_enabled(void) {
-    return caps_word_enabled || snake_case_enabled;
+    return caps_word_enabled || is_x_case_enabled();
 }
 
 void disable_caps_word(void) {
@@ -42,26 +61,30 @@ void disable_caps_word(void) {
     }
 }
 
-void toggle_caps_word(void) {
-    caps_word_enabled ? disable_caps_word() : enable_caps_word();
-}
-
-void toggle_snake_case(void) {
-    snake_case_enabled = !snake_case_enabled;
-}
-
-void toggle_screaming_snake_case(void) {
-    toggle_caps_word();
-    snake_case_enabled = !snake_case_enabled;
-}
-
 void disable_all(void) {
     disable_caps_word();
-    snake_case_enabled = false;
+    disable_x_case();
 }
 
 void disable_caps_all(void) {
     disable_all();
+}
+
+bool is_awaiting_separator(void) {
+    return x_case_state == AWAITING_SEPARATOR;
+}
+
+void resolve_separator(uint16_t keycode) {
+    separator = keycode;
+    x_case_state = X_CASE_ACTIVE;
+}
+
+void resolve_separator_default(void) {
+    resolve_separator(DEFAULT_SEPARATOR);
+}
+
+void apply_separator(void) {
+    tap_code16(separator);
 }
 
 bool process_caps_word(uint16_t keycode, keyrecord_t *record) {
@@ -69,54 +92,67 @@ bool process_caps_word(uint16_t keycode, keyrecord_t *record) {
 
     /* Return value determines if processing the keycode should continue to core code.
      * This allows more customizability. */
-    if (caps_word_enabled || snake_case_enabled) {
-        // first strip of the mod taps and layer taps if needed
-        switch (keycode) {
-            case QK_MOD_TAP ... QK_MOD_TAP_MAX:
-            case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
-                if (record->tap.count == 0)
-                    return true;
-                keycode = keycode & 0xFF;
+    if (!caps_word_enabled && !is_x_case_enabled()) {
+        return true;
+    }
+
+    if (!record->event.pressed) {
+        return true;
+    }
+
+    // first strip of the mod taps and layer taps if needed
+    switch (keycode) {
+        case QK_MOD_TAP ... QK_MOD_TAP_MAX:
+        case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
+            if (record->tap.count == 0)
+                return true;
+            keycode = keycode & 0xFF;
+    }
+
+    // we end caps word on one of the following keypresses
+    if (keycode == KC_SPC) {
+        if (is_awaiting_separator()) {
+            resolve_separator_default();
         }
 
-        // we end caps word on one of the following keypresses
-        switch (keycode) {
-            case KC_SPC:
-                if (snake_case_enabled) {
-                    // _ on space
-                    if (record->event.pressed) {
-                        if (!last_press_was_space) {
-                            last_press_was_space = true;
-                            register_code16(KC_UNDS);
-                        } else {
-                            last_press_was_space = false;
-                            disable_all();
-                            tap_code(KC_BSPC);
-                            return true;
-                        }
-                    } else {
-                        unregister_code16(KC_UNDS);
-                    }
-                    return false;
-                } else { // disable snake casing
-                    if (record->event.pressed) {
-                        disable_all();
-                    }
-                    return true;
-                }
-            case KC_ESC:
-            case KC_ENT:
-            case KC_TAB:
-            case KC_DOT:
-            case KC_COMM:
-                if (record->event.pressed) {
-                    last_press_was_space = false;
-                    disable_all();
-                }
-                return true;
-            default:
-                last_press_was_space = false;
+        if (last_press_was_space) {
+            last_press_was_space = false;
+            disable_all();
+            tap_code(KC_BSPC);
+            return true;
         }
+
+        apply_separator();
+        last_press_was_space = true;
+        return false;
+    }
+
+    last_press_was_space = false;
+
+    switch (keycode) {
+        case KC_DOT:
+        case KC_COMM:
+        case KC_TAB:
+            if (is_awaiting_separator()) {
+                resolve_separator(keycode);
+                return false;
+            }
+        case KC_ESC:
+        case KC_ENT:
+            if (record->event.pressed) {
+                disable_all();
+            }
+            return true;
+        case KC_A ... KC_0:
+            if (is_awaiting_separator()) {
+                resolve_separator_default();
+            }
+            return true;
+        default:
+            if (is_awaiting_separator()) {
+                resolve_separator(keycode);
+                return false;
+            }
     }
 
     return true;
--- keyboards/lily58/keymaps/baffalop/caps_word.h
+++ keyboards/lily58/keymaps/baffalop/caps_word.h
@@ -1,9 +1,6 @@
-void toggle_caps_word(void);
 void enable_caps_word(void);
-void enable_snake_case(void);
-void enable_screaming_snake_case(void);
+void enable_x_case(void);
+void enable_screaming_x_case(void);
 bool is_caps_anything_enabled(void);
-void toggle_snake_case(void);
-void toggle_screaming_snake_case(void);
 void disable_caps_all(void);
 bool process_caps_word(uint16_t keycode, keyrecord_t *record);
