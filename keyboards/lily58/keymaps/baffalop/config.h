0d112718829b467f48c0dea0b5f733b84367fe4a
Nikita Gaidakov <ngaidakov@podfather.com>
2021-03-18 21:07:02 +0000

    Add some underlighting and basic controls to keymap

--- keyboards/lily58/keymaps/baffalop/config.h
+++ keyboards/lily58/keymaps/baffalop/config.h
@@ -38,8 +38,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM_PER_KEY
 
 #undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 27
+#define RGBLED_NUM 6
+#define RGBLIGHT_SPLIT { 3, 3 }
+#define RGBLIGHT_DEFAULT_HUE 58
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_MOOD
 #define RGBLIGHT_LIMIT_VAL 120
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
