f7045055f81ee043b28f2ced1c853266d306c29e
Nikita Gaidakov <ngaidakov@podfather.com>
2021-05-31 22:44:38 +0100

    Move casemodes and combos into users directory for common use

--- keyboards/lily58/keymaps/baffalop/rules.mk
+++ keyboards/lily58/keymaps/baffalop/rules.mk
@@ -22,7 +22,8 @@ COMBO_ENABLE = yes
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
-SRC += casemodes.c
+SRC += ../../../../users/baffalop/casemodes.c \
+	   ../../../../users/baffalop/combos.c
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
