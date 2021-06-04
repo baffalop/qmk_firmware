45cc8947fcd44850d60e511e3af8d3a85503bd21
Nikita Gaidakov <ngaidakov@podfather.com>
2021-06-04 17:37:09 +0100

    Eliminate 6th column keys

--- keyboards/kyria/keymaps/baffalop/keymap.c
+++ keyboards/kyria/keymaps/baffalop/keymap.c
@@ -34,10 +34,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
     [_COLEMAK] = LAYOUT(
-        KC_TAB,     KC_Q,         KC_W,         KC_C,         KC_P,         KC_B,                                                                KC_J,    KC_L,         KC_U,         KC_Y,         KC_SLSH,      KC_MINS,
-        KC_ESC,     LSFT_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LGUI_T(KC_T), KC_G,                                                                KC_M,    RGUI_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), RSFT_T(KC_O), KC_QUOT,
-        KC_GRV,     KC_Z,         KC_X,         KC_F,         KC_D,         KC_V,   XXXXXXX,         XXXXXXX, XXXXXXX,         XXXXXXX,          KC_K,    KC_H,         KC_COMM,      KC_DOT,       KC_SCLN,      KC_BSLS,
-                                                XXXXXXX,      KC_CAPS,      KC_ESC, LT(_SYM, KC_NO), REPEAT,  RSFT_T(KC_ENT),  LT(_NAV, KC_SPC), KC_BSPC, KC_DEL,       TG(_QWERTY)
+        XXXXXXX,  KC_Q,         KC_W,         KC_C,         KC_P,         KC_B,                                                                KC_J,    KC_L,         KC_U,         KC_Y,         KC_SCLN,      XXXXXXX,
+        XXXXXXX,  LSFT_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LGUI_T(KC_T), KC_G,                                                                KC_M,    RGUI_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), RSFT_T(KC_O), XXXXXXX,
+        XXXXXXX,  KC_Z,         KC_X,         KC_F,         KC_D,         KC_V,   XXXXXXX,         KC_TAB,  XXXXXXX,         XXXXXXX,          KC_K,    KC_H,         KC_COMM,      KC_DOT,       KC_QUOT,      XXXXXXX,
+                                              KC_CAPS,      KC_TAB,       KC_ESC, LT(_SYM, KC_NO), REPEAT,  RSFT_T(KC_ENT),  LT(_NAV, KC_SPC), KC_BSPC, KC_DEL,       TG(_QWERTY)
     ),
                                          
     [_QWERTY] = LAYOUT(                  
